b2ff98dc4c322a56bc2a942b000ee491377d3f30
Ilya Safonov <safonovil17@yandex.ru>
2022-08-21 20:16:23 +0300

    Added solution of the third task

---
+++ Ex007_HW/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Программа покажет, является ли введенное число четным или нет");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0) {
+    Console.WriteLine($"Число {num} - чётное");
+} else {
+    Console.WriteLine($"Число {num} - нечётное");
+}
