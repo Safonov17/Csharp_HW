032e0ae406c2b81b1ecc1cceaf2226dfa1fc958b
Ilya Safonov <safonovil17@yandex.ru>
2022-08-21 20:06:04 +0300

    Added solutions of the second task

---
+++ Ex006_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Программа покажет, какое из трех целых чисел большее");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (num1 > num2) {
+    max = num1;
+} else {
+    max = num2;
+}
+if (num3 > max) {
+    max = num3;
+}
+
+
+Console.Write($"Максимальное число из введенных - {max}");
