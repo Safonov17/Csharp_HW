2d5987dfacb5fbb5d9d48d740dfc2b876ee8cfaa
Ilya Safonov <safonovil17@yandex.ru>
2022-08-21 20:25:17 +0300

    Added solution of the fourth task

---
+++ Ex008_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Программа для нахождения всех четных чисел от 1 до num");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1) {
+    Console.WriteLine("Ошибка. Введите целое положительное число, начиная с 1");
+} else {
+    for (int i = 2; i <= num; i +=2) {
+        Console.Write($"{i} ");
+    }
+}
