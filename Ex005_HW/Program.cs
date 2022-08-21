cf48d3a26b0965fcee23d7e0109ae507da49f1bb
Ilya Safonov <safonovil17@yandex.ru>
2022-08-21 19:49:35 +0300

    Added solution of the first task

--- Ex005_HW/Program.cs
+++ Ex005_HW/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Программа покажет, какое из двух целых чисел большее, а какое меньшее");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.WriteLine($"Число ({num1}) - большее");
+    Console.WriteLine($"Число ({num2}) - меньшее");
+} else {
+    Console.WriteLine($"Число ({num1}) - меньшее");
+    Console.WriteLine($"Число ({num2}) - большее");
+}
