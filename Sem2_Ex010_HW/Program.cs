5dab12a5dffa751024a3dc676424be343c10a924
Ilya Safonov <safonovil17@yandex.ru>
2022-08-25 14:10:08 +0300

    Second seminar. Added solution for the first task

---
+++ Sem2_Ex010_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int numAbs = Math.Abs(num);
+
+if (numAbs < 100 || numAbs > 999) {
+    Console.WriteLine("Введено неверное значение. Повторите попытку");
+} else {
+    string stringNum = Convert.ToString(numAbs);
+    Console.WriteLine($"Вторая цифра этого числа - {stringNum[1]}");
+}
