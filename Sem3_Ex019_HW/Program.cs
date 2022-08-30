440592d7f815cad45d5b5f0fd8e2200e69e1d800
Ilya Safonov <safonovil17@yandex.ru>
2022-08-30 21:34:07 +0300

    Added solutions to the first task from the third seminar

---
+++ Sem3_Ex019_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+    string num = Console.ReadLine();
+    string rev = string.Concat(num.Reverse());
+    Console.WriteLine(num == rev ? $"Число {num} - палиндром" : $"Число {num} - не палиндром");ЁЁ
