1ee0bd2c03a15293542b36398e10f954a4476806
Ilya Safonov <safonovil17@yandex.ru>
2022-08-31 17:50:04 +0300

    Added solution of the second task for the fourth seminar

---
+++ Sem4_Ex027_HW/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Math.Abs(int.Parse(Console.ReadLine()));
+int sum = 0;
+while (num > 0) {
+    sum = sum + num % 10;
+    num /= 10 ;
+}
+Console.WriteLine($"Сумма чисел -> {sum}");
