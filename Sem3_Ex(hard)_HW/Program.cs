50c32fe6c947d7378fe7597baf50bb50587f1678
Ilya Safonov <safonovil17@yandex.ru>
2022-08-31 12:04:33 +0300

    Added solution of the task with asterisk for the third seminar

---
+++ Sem3_Ex(hard)_HW/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая на вход принимает радиус круга и находит его 
+площадь, округленную до целого числа.
+Необходимо вывести максимальную цифру в полученном округлённом значении площади круга.
+10 -> 4
+20 -> 7
+30 -> 8
+*/
+Console.Clear();
+Console.Write("Введите значение радиуса круга: ");
+int rad = int.Parse(Console.ReadLine());
+int max = 0;
+
+// Находим значение площади круга по формуле
+double result = Math.Round(Math.PI * Math.Pow(rad, 2));
+Console.WriteLine($"Площадь круга равна -> {result}");
+
+// Находим максимальную цифру в получившемся числе
+int resultToInt = Convert.ToInt32(result);
+while (resultToInt > 0) {
+    if (max < resultToInt % 10) 
+        max = resultToInt % 10;
+        resultToInt /=10;
+}
+Console.WriteLine($"Максимальная цифра в этом числе -> {max}");
