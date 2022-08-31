87b3db737b531eba38a4e7c8d77b24a96c1e7b7f
Ilya Safonov <safonovil17@yandex.ru>
2022-08-31 17:06:09 +0300

    Added solution of the first task to the fourth seminar

---
+++ Sem4_Ex025_HW/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+*/
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int exp = Exp(num1, num2);
+Console.WriteLine($"{num1} ^ {num2} = {exp}");
+
+// Создаем метод, принимающий на вход два параметра
+int Exp(int num1, int num2)
+{
+    int result = 1;
+    // Проходимся циклом до тех пор, пока 
+    for (int i = 1; i <= num2; i++)
+        result = result * num1;
+    return result;
+}
