8ae63d9da5963f175c26803cb62a073010fd6b7c
Ilya Safonov <safonovil17@yandex.ru>
2022-09-02 18:28:05 +0300

    an updated commit message

---
+++ Sem5_Ex038_HW/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
+[3 7 22 2 78] -> 76
+*/
+Console.Clear();
+int[] array = new int[5];
+int max = 0;
+int min = 0;
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(-10, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++) {
+    if (array[j] > 0) {
+        max = array[j];
+    } else {
+        min = array[j];
+    }
+}
+Console.WriteLine($"{max} - ({min}) = {max - min}");
