161151bfb6c0c1e2a3065b3938f27960d4834cc9
I.Safonov <safonovil17@yandex.ru>
2022-09-12 10:16:28 +0300

    Added solution to 47 task

---
+++ Sem7_Ex047_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+*/
+Console.Clear();
+
+Console.Write("Укажите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Укажите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = new double[rows, columns];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
