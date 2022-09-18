abbfe5a5ac923b59469db911e642b20e1e2ad045
I.Safonov <safonovil17@yandex.ru>
2022-09-18 18:37:02 +0300

    Added solution 68th task

---
+++ Sem9_Ex068_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задайте значения M и N. 
+Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии
+M = 28; N = 7 -> 7
+*/
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int GCD(int x, int y) {
+   return y == 0 ? x : GCD(y, x % y);
+}
+
+int funcGCD = GCD(m, n);
+Console.WriteLine($"Наибольший общий делитель этих чисел => {funcGCD}");
